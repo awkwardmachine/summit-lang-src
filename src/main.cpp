df4789966128bff8c385f709a566eea18fe7c93d
awkwardmachine <noib2009@gmail.com>
2025-10-31 15:16:42 +0000

    Added run option to compiler for ease of use

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <llvm/Support/Error.h>
+
 #include "lexer/lexer.h"
 #include "parser/parser.h"
 #include "codegen/codegen.h"
-#include <llvm/Support/Error.h>
 
 // file reading utility
 std::string readFile(const std::string& filename) {
@@ -24,11 +25,12 @@ std::string readFile(const std::string& filename) {
 // cli help message
 void printUsage(const char* program_name) {
     std::cerr << "Usage: " << program_name << " [options] <input.sm>\n";
+    std::cerr << "       " << program_name << " run <filename/filename.sm>\n";
     std::cerr << "Options:\n";
     std::cerr << "  -o <output>    Output executable name\n";
     std::cerr << "  -c             Compile to object file only\n";
     std::cerr << "  -nostdlib      Do not link with standard library\n";
-    std::cerr << "  -nowindow      Do not show console window when using generated executable\n";
+    std::cerr << "  -window        Show console window when using generated executable\n";
     std::cerr << "  -h, --help     Show this help message\n";
 }
 
@@ -49,6 +51,19 @@ std::string getDefaultOutputName(const std::string& input_file, bool compile_onl
 #endif
 }
 
+std::string resolveInputFile(const std::string& input_arg) {
+    if (std::filesystem::exists(input_arg)) {
+        return input_arg;
+    }
+    
+    std::string with_ext = input_arg + ".sm";
+    if (std::filesystem::exists(with_ext)) {
+        return with_ext;
+    }
+    
+    throw std::runtime_error("Could not find file: " + input_arg + " or " + with_ext);
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         printUsage(argv[0]);
@@ -62,11 +77,21 @@ int main(int argc, char** argv) {
     bool compile_only = false;
     bool no_stdlib = false;
     bool no_window = true;
+    bool run_mode = false;
     
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
         
-        if (arg == "-h" || arg == "--help") {
+        if (arg == "run") {
+            run_mode = true;
+            if (i + 1 < argc) {
+                input_file = argv[++i];
+            } else {
+                std::cerr << "Error: run requires a filename\n";
+                return 1;
+            }
+            break;
+        } else if (arg == "-h" || arg == "--help") {
             printUsage(argv[0]);
             return 0;
         } else if (arg == "-o") {
@@ -84,7 +109,6 @@ int main(int argc, char** argv) {
         } else if (arg == "-window") {
             no_window = false;
         } else if (arg[0] != '-') {
-            // not a flag, must be the input file
             input_file = arg;
         } else {
             std::cerr << "Unknown option: " << arg << "\n";
@@ -98,8 +122,25 @@ int main(int argc, char** argv) {
         return 1;
     }
     
+    try {
+        input_file = resolveInputFile(input_file);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << "\n";
+        return 1;
+    }
+    
+    // set to run mode
+    if (run_mode) {
+        output_file = getDefaultOutputName(input_file, false);
+        if (std::filesystem::exists(output_file)) {
+            std::string run_cmd = output_file;
+            return std::system(run_cmd.c_str());
+        }
+        no_window = false;
+    }
+    
     // set default output name if needed
-    if (!output_specified) {
+    if (!output_specified && !run_mode) {
         output_file = getDefaultOutputName(input_file, compile_only);
     }
     
@@ -130,6 +171,12 @@ int main(int argc, char** argv) {
             }
             
             codegen.emitExecutable(output_file, libs, no_stdlib, no_window);
+            
+            if (run_mode) {
+                std::string run_cmd = output_file;
+                int result = std::system(run_cmd.c_str());
+                return result;
+            }
         }
         
         return 0;
